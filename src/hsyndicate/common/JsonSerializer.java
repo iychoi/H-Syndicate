69d77f9753e4a27d173f9f8e836848d8a7980b0c
Illyoung Choi <iychoi@email.arizona.edu>
2016-12-12 02:33:10 -0700

    Change license from princeton to arizona

--- src/hsyndicate/common/JsonSerializer.java
+++ src/hsyndicate/common/JsonSerializer.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 The Trustees of Princeton University
+   Copyright 2016 The Trustees of University of Arizona
 
    Licensed under the Apache License, Version 2.0 (the "License" );
    you may not use this file except in compliance with the License.
@@ -27,28 +27,28 @@ import org.codehaus.jackson.map.SerializationConfig;
  * @author iychoi
  */
 public class JsonSerializer {
-    
+
     private ObjectMapper mapper;
-            
+
     public JsonSerializer() {
         this.mapper = new ObjectMapper();
     }
-    
+
     public JsonSerializer(boolean prettyformat) {
         this.mapper = new ObjectMapper();
         this.mapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, prettyformat);
     }
-    
+
     public String toJson(Object obj) throws IOException {
         StringWriter writer = new StringWriter();
         this.mapper.writeValue(writer, obj);
         return writer.getBuffer().toString();
     }
-    
+
     public void toJsonFile(File f, Object obj) throws IOException {
         this.mapper.writeValue(f, obj);
     }
-    
+
     public Object fromJson(String json, Class<?> cls) throws IOException {
         if(json == null) {
             return null;
@@ -56,7 +56,7 @@ public class JsonSerializer {
         StringReader reader = new StringReader(json);
         return this.mapper.readValue(reader, cls);
     }
-    
+
     public Object fromJsonFile(File f, Class<?> cls) throws IOException {
         return this.mapper.readValue(f, cls);
     }
